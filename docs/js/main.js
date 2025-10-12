68edc4e9ac1edebd6d74977e8901926af5f47259
İbrahim Kaan Bilir <125301699+abes400@users.noreply.github.com>
2025-10-12 21:24:13 +0300

    Update main.js

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,10 +1,13 @@
+console.log("test");
 async function addPart(id, file) {
     const elmnt = document.getElementById(id);
-    const resp = await fetch(file);
+    const resp = await fetch(file).then((res) => {
+        res
+    });
     elmnt.innerHTML = await resp.text();
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("test");
+
     addPart('header', './header.html');
 });
