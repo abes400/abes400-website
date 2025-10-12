3a92d475878fa10febde44dac24ff14423b378be
İbrahim Kaan Bilir <125301699+abes400@users.noreply.github.com>
2025-10-12 21:05:57 +0300

    ADD header

---
+++ js/main.js
@@ -0,0 +1,9 @@
+async function addPart(id, file) {
+    const elmnt = document.getElementById(id);
+    const resp = await fetch(file);
+    elmnt.innerHTML = resp.text();
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    addPart('header', 'header.html');
+});
